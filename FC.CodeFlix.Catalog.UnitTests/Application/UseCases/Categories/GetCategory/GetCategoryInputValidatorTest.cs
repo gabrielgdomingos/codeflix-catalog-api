72f9f71ca57e216d4e34164dbb6b088be7b23878
Gabriel Domingos <gabrielgdomingos@gmail.com>
2022-06-04 19:09:03 -0300

    Implementado validação do GetCategoryInput com FluentValidation.

---
+++ FC.CodeFlix.Catalog.UnitTests/Application/UseCases/Categories/GetCategory/GetCategoryInputValidatorTest.cs
@@ -0,0 +1,48 @@
+﻿using FC.CodeFlix.Catalog.Application.UseCases.Categories.GetCategory;
+using FluentValidation.TestHelper;
+using System;
+using Xunit;
+
+namespace FC.CodeFlix.Catalog.UnitTests.Application.UseCases.Categories.GetCategory
+{
+    [Collection(nameof(GetCategoryUseCaseTestFixture))]
+    public class GetCategoryInputValidatorTest
+    {
+        private readonly GetCategoryUseCaseTestFixture _fixture;
+
+        public GetCategoryInputValidatorTest(GetCategoryUseCaseTestFixture fixture)
+            => _fixture = fixture;
+
+        [Fact(DisplayName = nameof(ValidateSuccess))]
+        [Trait("Application", "GetCategoryInputValidator - Use Cases")]
+        public void ValidateSuccess()
+        {
+            //Arrange
+            var input = new GetCategoryInput(Guid.NewGuid());
+
+            var validator = new GetCategoryInputValidator();
+
+            //Act
+            var result = validator.TestValidate(input);
+
+            //Assert
+            result.ShouldNotHaveAnyValidationErrors();
+        }
+
+        [Fact(DisplayName = nameof(InvalidWhenEmptyGuidId))]
+        [Trait("Application", "GetCategoryInputValidator - Use Cases")]
+        public void InvalidWhenEmptyGuidId()
+        {
+            //Arrange
+            var input = new GetCategoryInput(Guid.Empty);
+
+            var validator = new GetCategoryInputValidator();
+
+            //Act
+            var result = validator.TestValidate(input);
+
+            //Assert
+            result.ShouldHaveValidationErrorFor(x => x.Id);
+        }
+    }
+}
