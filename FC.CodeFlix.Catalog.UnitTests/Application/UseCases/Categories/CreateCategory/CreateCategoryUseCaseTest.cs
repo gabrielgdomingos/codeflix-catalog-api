72fe2a9823d2731315a6b4095caaaa034c1685c9
Gabriel Domingos <gabrielgdomingos@gmail.com>
2022-06-04 13:17:06 -0300

    Implementação teste do Use Case CreateCategory (falhando).

---
+++ FC.CodeFlix.Catalog.UnitTests/Application/UseCases/Categories/CreateCategory/CreateCategoryUseCaseTest.cs
@@ -0,0 +1,54 @@
+﻿using Xunit;
+
+namespace FC.CodeFlix.Catalog.UnitTests.Application.UseCases.Categories.CreateCategory
+{
+    public class CreateCategoryUseCaseTest
+    {
+        [Fact(DisplayName = nameof(CreateCategory))]
+        [Trait("Application", "CreateCategory - Use Cases")]
+        public async void CreateCategory()
+        {
+            //Arrange
+            //var repositoryMock = new Mock<ICategoryRepository>();
+
+            //var unitOfWorkMock = new Mock<IUnitOfWork>();
+
+            //var useCase = new CreateCategoryUseCase(
+            //    unitOfWorkMock.Object,
+            //    repositoryMock.Object
+            //);
+
+            //var input = new CreateCategoryInput(
+            //    "Category Name",
+            //    "Category Description",
+            //    true
+            //);
+
+            ////Act
+            //var output = await useCase.Handle(input, CancellationToken.None);
+
+            ////Assert
+            //output.Should().NotBeNull();
+            //output.Name.Should().Be("Category Name");
+            //output.Description.Should().Be("Category Description");
+            //output.Id.Should().NotBeEmpty();
+            //output.CreatedAt.Should().NotBe(default);
+            //output.IsActive.Should().BeTrue();
+
+            //repositoryMock.Verify(
+            //    repository => repository.Create(
+            //        It.IsAny<CategoryEntity>,
+            //        It.IsAny<CancellationToken>
+            //    ),
+            //    Times.Once
+            //);
+
+            //unitOfWorkMock.Verify(
+            //    unitOfWork => unitOfWork.Commit(
+            //        It.IsAny<CancellationToken>
+            //    ),
+            //    Times.Once
+            //);
+        }
+    }
+}
