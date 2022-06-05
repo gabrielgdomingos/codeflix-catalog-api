a535a4ee453cbf849a18e7c4f2776f1661f628cf
Gabriel Domingos <gabrielgdomingos@gmail.com>
2022-06-05 20:24:36 -0300

    Implementado o DbContext e o Mapeamento da CategoryEntity.

---
+++ FC.CodeFlix.Catalog.Infrastructure.Persistence.EF/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using FC.CodeFlix.Catalog.Domain.Entities.Categories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FC.CodeFlix.Catalog.Infrastructure.Persistence.EF.Configurations
+{
+    internal class CategoryConfiguration
+        : IEntityTypeConfiguration<CategoryEntity>
+    {
+        public void Configure(EntityTypeBuilder<CategoryEntity> builder)
+        {
+            builder.HasKey(category => category.Id);
+
+            builder.Property(category => category.Name)
+                .HasMaxLength(255);
+
+            builder.Property(category => category.Description)
+                .HasMaxLength(10000);
+        }
+    }
+}
---
+++ FC.CodeFlix.Catalog.Infrastructure.Persistence.EF/DbContexts/CodeFlixCatalogDbContext.cs
@@ -0,0 +1,21 @@
+﻿using FC.CodeFlix.Catalog.Domain.Entities.Categories;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace FC.CodeFlix.Catalog.Infrastructure.Persistence.EF.DbContexts
+{
+    public class CodeFlixCatalogDbContext
+        : DbContext
+    {
+        public DbSet<CategoryEntity> Categories => Set<CategoryEntity>();
+
+        public CodeFlixCatalogDbContext(DbContextOptions<CodeFlixCatalogDbContext> options)
+            : base(options)
+        { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
