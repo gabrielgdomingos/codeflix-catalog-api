47d15ed8bfbb3f558bce70cab3f2ecbf27e52e05
Gabriel Domingos <gabrielgdomingos@gmail.com>
2022-08-13 15:26:28 -0300

    Implementado o método ThrowIfNull na classe NotFoundException

--- FC.CodeFlix.Catalog.Application/Exceptions/NotFoundException.cs
+++ FC.CodeFlix.Catalog.Application/Exceptions/NotFoundException.cs
@@ -5,5 +5,11 @@
     {
         public NotFoundException(string? message)
             : base(message) { }
+
+        public static void ThrowIfNull(object? @object, string? message)
+        {
+            if (@object == null)
+                throw new NotFoundException(message);
+        }
     }
 }
